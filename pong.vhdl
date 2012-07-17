e257a0a669697d802809cd7861b47bc1e4888c1c
Bhaallord <Bhaallord@arcor.de>
2012-07-17 16:42:35 +0200

    Leere Hauptentity angelegt.

---
+++ pong.vhdl
@@ -0,0 +1,19 @@
+
+-------------------------------------------------------
+--! @file
+--! @brief PONG Hauptentity 
+-------------------------------------------------------
+entity PONG is
+    port(
+        clk: in bit
+    );
+end entity;
+
+
+architecture PONG_ARC of PONG is
+
+
+begin
+
+
+end architecture;
