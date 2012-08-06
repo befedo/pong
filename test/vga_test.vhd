c015bd408ce6aeb90c5760b7c34fe87af7bb2f39
Ludwig <marc@localhost.localdomain>
2012-08-06 19:55:11 +0200

    Testbench korrigiert.

--- test/vga_test.vhd
+++ test/vga_test.vhd
@@ -11,13 +11,13 @@ use  IEEE.STD_LOGIC_UNSIGNED.all;
 entity VGA_TEST is end entity VGA_TEST;
 
 
-architecture ARCH of entity is 
+architecture ARCH of VGA_TEST is 
 
 signal	CLK, SIG_RED, SIG_GREEN, SIG_BLUE			:		bit;
 signal	SIG_RED_OUT, SIG_GREEN_OUT, SIG_BLUE_OUT, 
 		SIG_H_SYNC_OUT, SIG_V_SYNC_OUT, 
 		SIG_VIDEO_ON, SIG_PIXEL_CLOCK				: 		std_logic;
-signal	SIG_PIXEL_ROW, SIG_PIXEL_COLUMN				: 		std_logic_vector(9 downto 0));
+signal	SIG_PIXEL_ROW, SIG_PIXEL_COLUMN				: 		std_logic_vector(9 downto 0);
 
 component VGA is   
 	  port(	CLOCK_50Mhz, RED, GREEN, BLUE			: 	in	std_logic;
